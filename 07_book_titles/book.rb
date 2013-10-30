662cc7fc4c6c847822bd3f76d876f24d442d2b45
Matt Gray <matt@mattgray.ca>
2013-10-30 15:07:50 -0400

    test7

--- 07_book_titles/book.rb
+++ 07_book_titles/book.rb
@@ -1,8 +1,14 @@
 class Book
   attr_accessor :title
 
-  def title=(string)
-    string.each { |word| word.capitalize! }
+    def title
+     @title = title
+    end
+ 
+    def title
+     if @title.split(" ").length < 4 then
+     @title.split(" ").each {|x| print x.capitalize! }.join(" ")
+     else
+    end
   end
-
 end
